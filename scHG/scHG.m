f58f1aad9612c4334eecc7ea703b2f315177d375
anchor-hue <77482074+anchor-hue@users.noreply.github.com>
2025-09-05 12:05:35 +0800

    Update and rename ebmgc_gnf.m to scHG.m

--- scHG/scHG.m
+++ scHG/scHG.m
@@ -1,13 +1,5 @@
-function [Y, obj, coeff] = ebmgc_gnf(As, Y, grid_cnt, max_iter)
+function [Y, obj, coeff] = scHG(As, Y, grid_cnt, max_iter)
 
-%   Wu D, Yang Z, Lu J, et al. EBMGC-GNF: Efficient Balanced Multi-view Graph Clustering via Good Neighbor Fusion[J]. 
-%   IEEE Transactions on Pattern Analysis and Machine Intelligence, 2024.
-%
-%   Please contact Danyang Wu <danyangwu.cs@gmail.com> if you have any
-%   questions.
-%
-%   SPDX-FileCopyrightText: 2024 Zhenkun Yang <ftyzknow@mail.scut.edu.cn>
-%   SPDX-License-Identifier: MIT
     arguments
         As
         Y
